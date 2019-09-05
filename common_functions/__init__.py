c4f0355c0587b9f56468962c30a176416c2c98e6
Aaron England <aaron.england24@gmail.com>
2019-09-05 15:53:55 -0700

    removed lift and added recommendations

--- common_functions/__init__.py
+++ common_functions/__init__.py
@@ -1,23 +1,47 @@
 # common functions
 
-# function to calculate lift (i.e., association)
-def get_lift(N_item_1, N_item_2, N_both_items, N_transactions):
-    # calculate probability of buying 1 and 2 if item 2 is bought
-    confidence = N_both_items/N_item_2
-    # caculate overall probability of buying item 1
-    support = N_item_1/N_transactions
-    # calculate increase in ratio of item 2 when item 1 is sold
-    lift = confidence/support
-    # def class of attributes
-    class attributes:
-        def __init__(self, confidence, support, lift):
-            self.confidence = confidence
-            self.support = support
-            self.lift = lift
-    # save class as returnable object
-    attributes = attributes(confidence, support, lift)
-    # return the object
-    return attributes
+# define a function for recommendations
+def recommendations(treatment_plan_id, available_at, product_name, name_of_product):
+    # create df
+    df['treatment_plan_id'] = treatment_plan_id
+    df['available_at'] = available_at
+    df['product_name'] = product_name
+    # concatenate treatment_plan_id and date and make into a col so we can make sure the same tx plan and same available_at date are grouped
+    df['tx_plan_id_date'] = df.apply(lambda x: str(x['treatment_plan_id']) + ' ' + str(x['available_at']), axis=1)
+    # get the unique values for tx_plan_id_date
+    list_tx_plan_id_date = list(pd.value_counts(df['tx_plan_id_date']).index)
+    # make a list of product_name for each tx_plan_id_date (this could be time consuming)
+    time_start = datetime.datetime.now()
+    counter = 1
+    list_list_product_name = []
+    for tx_plan_id_date in list_tx_plan_id_date:
+        # subset to just that tx_plan_id_date
+        df_subset = df[df['tx_plan_id_date'] == tx_plan_id_date]
+        # put product_name into a list
+        list_product_name = list(df_subset['product_name'])
+        # remove any duplicates
+        list_product_name = list(dict.fromkeys(list_product_name))
+        # append list_product_name to list_list_product_name
+        list_list_product_name.append(list_product_name)
+        # get current time
+        time_current = datetime.datetime.now()
+        # get minutes elapsed from time_start
+        time_elapsed = (time_current - time_start).seconds/60
+        # print a message to the console for status
+        stdout.write('\r{0}/{1}; {2:0.4f}% complete; elapsed time: {3:0.2} min.'.format(counter, len(list_tx_plan_id_date), (counter/len(list_tx_plan_id_date))*100, time_elapsed))
+        stdout.flush()
+        # increase counter by 1
+        counter += 1
+    # get only the lists containing name_product from list_list_product_name and combine them into a large list
+    list_list_list_product_name = []
+    for list_product_name in list_list_product_name:
+        if name_product in list_product_name:
+            list_list_list_product_name.extend(list_product_name)
+    # Get top 10 value counts (excluding the first index because it will be itself)
+    print('\n')
+    print('Most commonly prescribed with {0}:'.format(name_product))
+    for i in range(1, 11):
+        print('{0}. {1}'.format(i, pd.value_counts(list_list_list_product_name).index[i]))
 
 # get month name from month number
 def get_month_name(month_number):
