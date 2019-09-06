ac844ede0d2b39a3c857b7d4a4693a5f803c5037
Aaron England <aaron.england24@gmail.com>
2019-09-06 12:36:19 -0700

    Update __init__.py
    replaced recommendations function

--- common_functions/__init__.py
+++ common_functions/__init__.py
@@ -1,7 +1,6 @@
 # common functions
-from sys import stdout
 import pandas as pd
-import datetime
+import numpy as np
 
 # get month name from month number
 def get_month_name(month_number):
@@ -58,46 +57,50 @@ def max_days_month(month_number):
         return 31
 
 # define a function for recommendations
-def recommendations(arr_treatment_plan_id, arr_available_at, arr_product_name, name_of_product, n_commonly_prescribed=10):
-    # make empty df
+# make function
+def recommendations(arr_transaction, arr_product_name, arr_modality, list_target_products, target_modality, modality=True):
+    # create empty df
     df = pd.DataFrame()
-    # create df
-    df['treatment_plan_id'] = arr_treatment_plan_id
-    df['available_at'] = arr_available_at
+    # put arrays as cols in df
+    df['transaction'] = arr_transaction
     df['product_name'] = arr_product_name
-    # concatenate treatment_plan_id and date and make into a col so we can make sure the same tx plan and same available_at date are grouped
-    df['tx_plan_id_date'] = df.apply(lambda x: str(x['treatment_plan_id']) + ' ' + str(x['available_at']), axis=1)
-    # get the unique values for tx_plan_id_date
-    list_tx_plan_id_date = list(pd.value_counts(df['tx_plan_id_date']).index)
-    # make a list of product_name for each tx_plan_id_date (this could be time consuming)
-    time_start = datetime.datetime.now()
-    counter = 1
-    list_list_product_name = []
-    for tx_plan_id_date in list_tx_plan_id_date:
-        # subset to just that tx_plan_id_date
-        df_subset = df[df['tx_plan_id_date'] == tx_plan_id_date]
-        # put product_name into a list
-        list_product_name = list(df_subset['product_name'])
-        # remove any duplicates
-        list_product_name = list(dict.fromkeys(list_product_name))
-        # append list_product_name to list_list_product_name
-        list_list_product_name.append(list_product_name)
-        # get current time
-        time_current = datetime.datetime.now()
-        # get minutes elapsed from time_start
-        time_elapsed = (time_current - time_start).seconds/60
-        # print a message to the console for status
-        stdout.write('\r{0}/{1}; {2:0.4f}% complete; elapsed time: {3:0.2} min.'.format(counter, len(list_tx_plan_id_date), (counter/len(list_tx_plan_id_date))*100, time_elapsed))
-        stdout.flush()
-        # increase counter by 1
-        counter += 1
-    # get only the lists containing name_product from list_list_product_name and combine them into a large list
-    list_list_list_product_name = []
-    for list_product_name in list_list_product_name:
-        if name_of_product in list_product_name:
-            list_list_list_product_name.extend(list_product_name)
-    # Get top 10 value counts (excluding the first index because it will be itself)
+    df['modality'] = arr_modality
+    
+    # set up logic for modality
+    if modality == True:
+        # subset modality
+        df = df[df['modality'] == target_modality]
+    # drop modality
+    df.drop(['modality'], axis=1, inplace=True)
+    
+    # convert into df with lists
+    df = df.groupby('transaction').agg(lambda x: x.unique().tolist()).reset_index()
+    # drop prescription col
+    df.drop(['transaction'], axis=1, inplace=True)
+    
+    # mark rows with single item
+    df['single_item'] = df.apply(lambda x: 1 if len(x['product_name']) == 1 else 0, axis=1)
+    # drop rows with single items
+    df = df[df['single_item'] == 0]
+    # drop single_item col
+    df.drop(['single_item'], axis=1, inplace=True)
+    
+    # mark rows with 1 if target product in list
+    df['target_item'] = df.apply(lambda x: 1 if set(list_target_products).issubset(set(x['product_name'])) else 0, axis=1)
+    # drop rows where target_item == 0
+    df = df[df['target_item'] == 1]
+    # drop target_item col
+    df.drop(['target_item'], axis=1, inplace=True)
+    
+    # flatten list
+    list_product_names = list(itertools.chain(*list(df['product_name'])))
+    # get value counts
+    list_suggested_items = list(pd.value_counts(list_product_names).index)
+    # get the items in list_suggested_items not in list_products_of_choice
+    final_suggested_items = np.setdiff1d(list_suggested_items, list_products_of_choice, assume_unique=True)
+
+    # print suggested items list
     print('\n')
-    print('{0} most commonly prescribed with {1}:'.format(n_commonly_prescribed, name_of_product))
-    for i in range(1, n_commonly_prescribed+1):
-        print('{0}. {1}'.format(i, pd.value_counts(list_list_list_product_name).index[i]))
+    print('Item(s) frequently associated with {0} for a {1}:'.format(list_target_products, target_modality))
+    for i in range(len(final_suggested_items)):
+        print('{0}. {1}'.format(i+1, final_suggested_items[i]))
